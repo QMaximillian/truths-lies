640f995a183f68dab3085891048b654e24817792
QMaximillian <quinnlashinsky@gmail.com>
2018-07-31 11:15:13 -0400

    Name form working

--- js-frontend/index.js
+++ js-frontend/index.js
@@ -1,2 +1,28 @@
 console.log("hello world")
 fetch("http://localhost:3000/api/v1/players").then( res => res.json()).then(console.log)
+
+const nameField = document.getElementById('name')
+// const optOneField = document.getElementById('1')
+// const optTwoField = document.getElementById('2')
+// const optThreeField = document.getElementById('3')
+const playerForm = document.getElementById('player-form')
+
+playerForm.addEventListener('submit', () => handleSubmit(event, nameField))
+
+function handleSubmit(e, nameField){
+e.preventDefault();
+if (nameField.value.length > 0){
+    return fetch('http://localhost:3000/api/v1/players', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name: nameField.value
+      })
+    })
+
+  }
+
+}
