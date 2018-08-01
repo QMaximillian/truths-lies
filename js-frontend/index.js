b9f9cff461a994388dc88da190a1f901094e36b6
QMaximillian <quinnlashinsky@gmail.com>
2018-08-01 15:49:33 -0400

    Basic functionality achieved

--- js-frontend/index.js
+++ js-frontend/index.js
@@ -9,6 +9,8 @@ const optOne = document.getElementById('1')
 const optTwo = document.getElementById('2')
 const optThree = document.getElementById('3')
 const quizCardArea = document.getElementById('quiz-card-area')
+const answerObject = {}
+let counter = 0
 playerForm.addEventListener('submit', handleNameSubmit)
 
 
@@ -51,7 +53,7 @@ function showForm(resp){
     nameForm.className = "hidden"
     form.className = "visible"
     form.dataset.id = resp.data.id
-  }
+}
 
 form.addEventListener('submit', handleFormSubmit)
 
@@ -70,72 +72,79 @@ function getNumber(){
 }
 
   function handleFormSubmit(event){
-    event.preventDefault();
-    promptUserForNumber();
+  event.preventDefault();
+  promptUserForNumber();
   }
 
   function handleFormSubmitPartTwo(fon){
-    return fetch(QUIZ_URL, {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        player_id: form.dataset.id,
-        option1: optOne.value,
-        option2: optTwo.value,
-        option3: optThree.value,
-        false_option: fon
-      })
-    }).then(resp => resp.json()).then(console.log)
-  }
-
-  function fetchCards(){
-      return fetch(QUIZ_URL).then( res => res.json()).then(formatCards)
-  }
-
-  function formatCards(response){
-    response.data.forEach(function(card){
-      quizCardArea.innerHTML += cardTemplate(card)
+  return fetch(QUIZ_URL, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      player_id: form.dataset.id,
+      option1: optOne.value,
+      option2: optTwo.value,
+      option3: optThree.value,
+      false_option: fon
     })
+  }).then(resp => resp.json()).then(console.log)
+  }
+
+function fetchCards(){
+    return fetch(QUIZ_URL).then( res => res.json()).then(formatCards)
+}
+
+function formatCards(response){
+  response.data.forEach(function(card){
+    answerObject[card.id] = card.attributes["false-option"]
+    quizCardArea.innerHTML += cardTemplate(card)
+    counter.className = ""
+  })
+  counter = 0
+  quizCardArea.addEventListener('click', handleNameSelect)
+}
+
+function cardTemplate(card){
+  return `<div data-card="${card.id}" class="cards">
+    <div data-player="${card.attributes.player.id}" class="player-name">
+      ${card.attributes.player.name}
+    </div>
+    <div data-options="${card.id}" class="hidden">
+      <ul class="options">
+      <li name="1">${card.attributes.option1} </li>
+      <li name="2">${card.attributes.option2} </li>
+      <li name="3">${card.attributes.option3} </li>
+      </ul>
+    </div>
+  </div> `
+}
+
+function handleNameSelect(e){
+
+  if (e.target.className === "player-name"){
+  let currentCard = e.target.parentNode.childNodes[3]
+  let cardNumber = parseInt(currentCard.dataset.options)
+  let currentCardOptions = currentCard.querySelector('ul')
+  console.log(currentCardOptions)
+  // let allCards = document.querySelectorAll('.player-name')
+  currentCard.className = ""
+        quizCardArea.removeEventListener('click', handleNameSelect)
+        currentCardOptions.addEventListener('click', () => handleAnswerSelect(event, cardNumber))
+    }
+}
+
+function handleAnswerSelect(e, cardNumber){
+  const answerNumber = parseInt(e.target.attributes.name.value)
+  if (answerObject[cardNumber] === answerNumber){
+    console.log('You\'re right!')
+    const currentCard = document.querySelector(`[data-card="${cardNumber}"]`)
+    currentCard.className = "hidden"
+    counter++
     quizCardArea.addEventListener('click', handleNameSelect)
+  } else {
+    console.log("you're wrong!")
   }
-
-  function handleNameSelect(e){
-    // console.log(e.target)
-    // if (e.target.className === "player-name"){
-    //   let selectedCard = e.target.dataset.player;
-    //   let allCards = document.querySelectorAll('.player-name')
-    //   allCards.forEach(function(card){
-    //
-    //   })
-    // }
-    if (e.target.className === "player-name"){
-    let currentCard = e.target.parentNode.childNodes[3]
-    // let allCards = document.querySelectorAll('.player-name')
-    currentCard.className = ""
-          quizCardArea.removeEventListener('click', handleNameSelect)
-  }
-    // allCards.forEach(function(card){
-    //   if(card.attributes[0].nodeValue !== currentCard.parentNode.dataset.card){
-    //     card.className = 'player-name hidden'
-    //   }
-    // })
-  }
-
-
-  function cardTemplate(card){
-    return `<div data-card="${card.id}" class="cards">
-      <div data-player="${card.attributes.player.id}" class="player-name">
-        ${card.attributes.player.name}
-      </div>
-      <div data-options="${card.id}" class="hidden">
-        <ul class="options">
-        <li>${card.attributes.option1} </li>
-        <li>${card.attributes.option2} </li>
-        <li>${card.attributes.option3} </li>
-        </ul>
-      </div>
-    </div> `
-  }
+}
