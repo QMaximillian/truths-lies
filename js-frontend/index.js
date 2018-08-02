f4869aaba168760775d950a991d108ea1dbb0f56
katrinagarloff <katrinagarloff@gmail.com>
2018-08-02 12:09:47 -0400

    the ugly truth

--- js-frontend/index.js
+++ js-frontend/index.js
@@ -9,9 +9,12 @@ const optOne = document.getElementById('1')
 const optTwo = document.getElementById('2')
 const optThree = document.getElementById('3')
 const quizCardArea = document.getElementById('quiz-card-area')
-const answerObject = {}
+const userObject = {}
 const signUpText = document.getElementById('sign-up')
 const selectPlayerForm = document.getElementById('select-player-form')
+const gameOverDiv = document.getElementById('game-over')
+let playerId = 0
+let currentPlayerHighScore = 0
 let counter = 0
 nameForm.addEventListener('submit', handleNameSubmit)
 signUpText.addEventListener('click', handleSignUp)
@@ -50,7 +53,7 @@ if (nameField.value.length > 0){
     }).then(resp => resp.json()).then(showForm)
   }
   else {
-    const playerId = parseInt(dropDown.value)
+    playerId = parseInt(dropDown.value)
     nameForm.className = "hidden"
     fetchCards(playerId)
   }
@@ -60,6 +63,7 @@ function showForm(resp){
     nameForm.className = "hidden"
     form.className = "visible"
     form.dataset.id = resp.data.id
+    playerId = resp.data.id
 }
 
 form.addEventListener('submit', handleFormSubmit)
@@ -120,10 +124,18 @@ function fetchCards(cardUserId){
 function formatCards(response, cardUserId){
   response.data.forEach(function(card){
     if (cardUserId !== card.attributes.player.id){
-    answerObject[card.id] = card.attributes["false-option"]
+    userObject[card.id]= {
+      "falseOption": card.attributes["false-option"],
+      "highScore": card.attributes.player.high_score}
+
     quizCardArea.innerHTML += cardTemplate(card)
-    counter.className = ""
+  } else {
+      currentPlayerHighScore = card.attributes.player.high_score
     }
+
+    // userObject[card.id] ["falseOption"] = card.attributes["false-option"]
+    // userObject[card.id]["highScore"] = card.attributes.player["highest-score"]
+
   })
   counter = 0
   quizCardArea.addEventListener('click', handleNameSelect)
@@ -160,14 +172,66 @@ function handleNameSelect(e){
 
 function handleAnswerSelect(e, cardNumber){
   const answerNumber = parseInt(e.target.attributes.name.value)
-  if (answerObject[cardNumber] === answerNumber){
+  if (userObject[cardNumber]["falseOption"] === answerNumber){
+
     console.log('You\'re right!')
     const currentCard = document.querySelector(`[data-card="${cardNumber}"]`)
     currentCard.className = "hidden"
     counter++
-      // if (counter != answerObject.values.length)
         quizCardArea.addEventListener('click', handleNameSelect)
   } else {
-    console.log("you're wrong!")
+    if(counter > currentPlayerHighScore){
+    handlePatchRequest()
+  } else {
+    displayGameOver()
   }
+
+
+  }
+}
+
+function handlePatchRequest(){
+  return fetch(PLAYER_URL +`/${playerId}`, {
+    method: 'PATCH',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+    high_score: counter
+    })
+  }).then(displayGameOver)
+
+}
+function displayGameOver(){
+  console.log("you're wrong!")
+
+  fetch(PLAYER_URL).then( res => res.json()).then(function(resp){
+    let highScores = []
+    let playerName
+    let playerHighScore
+    resp.data.forEach(function(player){
+      let newObj = {}
+      playerName = player.attributes.name
+      playerHighScore = player.attributes['high-score']
+      newObj["name"] = `${playerName}`
+      newObj["score"] = playerHighScore
+      highScores.push(newObj)
+      })
+      let sortedScores = highScores.sort(function(a, b){
+       return b.score - a.score
+      })
+      gameOverDiv.innerHTML += `<h1>"LEADERBOARD"</h1>`
+      for(let i=0; i<6; i++){
+        gameOverDiv.innerHTML += `<ul>
+        <li>${sortedScores[i].name} ${sortedScores[i].score}</li>
+        </ul>`
+      }
+    })
+  quizCardArea.className = "hidden"
+
+  gameOverDiv.innerHTML += `<div id="player-info">
+
+  </div>`
+  gameOverDiv.className = ""
 }
